ae80cee9b1257239a6bae8c554e0226cfdd8e5f5
Sina Ebrahimi <Ebrahimisina78@gmail.com>
2019-08-11 16:16:04 +0430

    testttt

--- bot.py
+++ bot.py
@@ -39,11 +39,8 @@ def scrap_song_url(url):
 
 # def get_current_song_info():
 #     return {'artist': 'halsey', 'title': 'Nightmare'}
-# @bot.message_handler(commands=['lyric'])
-# def slyric(message):
-#     artist = message.text.split()[1]
-#     song = message.text.split()[2]
-#     return
+
+
 
 # def main():
 #     args_length = len(sys.argv)
@@ -85,25 +82,28 @@ def scrap_song_url(url):
 
 @bot.message_handler(commands=['lyric'])
 def send(message):
-    artist = message.text.split()[1]
-    song = message.text.split()[2]
-    current_song_info = {'artist': '{}'.format(artist), 'title': '{}'.format(song)}
-    song_title = current_song_info['title']
-    artist_name = current_song_info['artist']
-    bot.send_message(message.chat.id, "{} by {}".format(song_title, artist_name))
-    # Search for matches in request response
-    response = request_song_info(song_title, artist_name)
-    json = response.json()
-    remote_song_info = None
+    if message.text.split()[1] and message.text.split()[2]:
+        artist = message.text.split()[1]
+        song = message.text.split()[2]
+        current_song_info = {'artist': '{}'.format(artist), 'title': '{}'.format(song)}
+        song_title = current_song_info['title']
+        artist_name = current_song_info['artist']
+        bot.send_message(message.chat.id, "{} by {}".format(song_title, artist_name))
+        # Search for matches in request response
+        response = request_song_info(song_title, artist_name)
+        json = response.json()
+        remote_song_info = None
 
-    for hit in json['response']['hits']:
-        if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
-            remote_song_info = hit
-            break
-    if remote_song_info:
-        song_url = remote_song_info['result']['url']
-        lyrics = scrap_song_url(song_url)
-    bot.send_message(message.chat.id, lyrics)
+        for hit in json['response']['hits']:
+            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
+                remote_song_info = hit
+                break
+        if remote_song_info:
+            song_url = remote_song_info['result']['url']
+            lyrics = scrap_song_url(song_url)
+        bot.send_message(message.chat.id, lyrics)
+    else:
+        bot.send_message(message.chat.id, "wrong format")
 
 
 
